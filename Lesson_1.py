7d972bc909c56a1be3ad8a648d10a548b335190a
Nadejda Maslenitsyna <nmaslen81@gmail.com>
2019-12-15 09:08:24 +0500

    Add change

--- Lesson_1.py
+++ Lesson_1.py
@@ -1,2 +1,20 @@
 import requests
-print ('ok')
+import json
+
+#Tast 1
+#username - ник на githab
+#выводит список репозиториев
+username = 'grayhor'
+req = requests.get('https://api.github.com/users/' + username+ '/repos')
+data = req.json()
+for i in range(len(data)):
+    print(data[i]['name'])
+
+#task 2
+
+req = requests.get('https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=godfather&api-key=9fu5gRSfku6xrIV0WecVddS26VAAhscu')
+print(req.status_code)
+data_movie = req.json()
+#print(json.dump(data_movie, indent=4, sort_keys=True))
+with open('movie.json', 'w') as f:
+    json.dump(data_movie, f, indent=4, sort_keys= True)
